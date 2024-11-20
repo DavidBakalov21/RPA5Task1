3858bde8b065d4cf1971621f469966e61a95ada6
DavidBakalov21 <davidbakalov33@gmail.com>
2024-11-20 20:31:55 +0200

    Initial Commit

---
+++ RPA5/RPA5App.swift
@@ -0,0 +1,17 @@
+//
+//  RPA5App.swift
+//  RPA5
+//
+//  Created by david david on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RPA5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPA5Tests/RPA5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RPA5Tests.swift
+//  RPA5Tests
+//
+//  Created by david david on 20.11.2024.
+//
+
+import Testing
+@testable import RPA5
+
+struct RPA5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
